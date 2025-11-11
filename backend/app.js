f4cb9e2d9870577720b972ca13b3d7d090c7525f
dzemil karisik <karisikdzemil@gmail.com>
2025-11-11 20:37:14 +0100

    connected with mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const db_key = "mongodb+srv://karisikdzemil:Dzemil123@cluster0.ldrhrp1.mongodb.net/havenspace?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
 
-app.listen(8080);
+mongoose.connect(db_key).then(result => {
+
+    app.listen(8080);
+}).catch(err => console.log(err))
