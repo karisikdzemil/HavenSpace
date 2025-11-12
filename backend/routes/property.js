949fcdc4d16da63b936afa68f55c0b986eb9270a
dzemil karisik <karisikdzemil@gmail.com>
2025-11-12 18:55:32 +0100

    created routes for mvp of project

---
+++ backend/routes/property.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const propertyController = require('../controllers/propertyController');
+
+const router = express.Router();
+
+router.get('/', );
+
+// router.get('/properties');
+
+// router.get('/property');
+
+// router.post('/property');
+
+// router.put('/property');
+
+// router.delete('/property');
+
+module.exports = router;
