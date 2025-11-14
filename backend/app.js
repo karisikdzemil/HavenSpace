504bdd805158c6e2f1fcf77844f45ca55b637e16
dzemil karisik <karisikdzemil@gmail.com>
2025-11-14 08:08:41 +0100

    user created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const propertyRoutes = require('./routes/property');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const propertyRoutes = require("./routes/property");
+const User = require("./models/user");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 const db_key = process.env.MONGO_URI;
 
 const app = express();
@@ -11,15 +12,36 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api', propertyRoutes);
+app.use("/api", propertyRoutes);
 
 app.use((error, req, res, next) => {
-    const message = error.message || 'Something went wrong!';
-    const status = error.statusCode || 500;
-    res.status(status).json({message: message});
+  const message = error.message || "Something went wrong!";
+  const status = error.statusCode || 500;
+  res.status(status).json({ message: message });
 });
 
-mongoose.connect(db_key).then(result => {
-    console.log('Connected with mongodb!');
+mongoose
+  .connect(db_key)
+  .then((result) => {
+    console.log("Connected with mongodb!");
+    User.find()
+      .then((users) => {
+        console.log(users)
+        if (users.length === 0) {
+          const user = new User({
+            email: "karisikdzemil@gmail.com",
+            password: "ljuljaska!2",
+          });
+          user
+            .save()
+            .then((user) => {
+              console.log(user);
+              app.listen(8080);
+            })
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
     app.listen(8080);
-}).catch(err => console.log(err))
+  })
+  .catch((err) => console.log(err));
