d29bcb05e8975b13235dae89cf30fed1100cfa10
dzemil karisik <karisikdzemil@gmail.com>
2025-12-16 20:24:45 +0100

    api connected with frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,35 @@
+import { useEffect } from "react";
 import HomeDiscover from "../components/home/homeDiscover";
 import HomeHeroSection from "../components/home/homeHeroSection";
 import AskedQeustions from "../components/home/questionsSections";
-import PropertyListingsSections from "../components/propertyListingsSection";
+import PropertyListingsSection from "../components/propertyListingsSection";
 
-export default function Home() {
+export default function Home() {  
+
+ useEffect(() => {
+    const getProperties = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/api/properties");
+
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    getProperties();
+  }, []);
+
   return (
     <>
       <HomeHeroSection />
       <HomeDiscover />
-      <PropertyListingsSections />
+      <PropertyListingsSection />
       <AskedQeustions />
     </>
   );
