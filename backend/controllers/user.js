cab2f889636bd7fb13ff297c30df7fe368ef98f5
dzemil karisik <karisikdzemil@gmail.com>
2025-12-22 20:55:25 +0100

    started with user model

---
+++ backend/controllers/user.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+
+exports.getUser = (req, res, next) => {
+
+}
+
+exports.postUser = (req, res, next) => {
+
+}
+
+exports.editUser = (req, res, next) => {
+
+}
+
+exports.deleteUser = (req, res, next) => {
+
+}
---
+++ backend/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userRoutes = require('../controllers/user');
+
+const router = express.Router();
+
+router.get('/user', );
+
+router.post('/create-user');
+
+router.delete('/delete-user');
+
+router.put('/edit-user');
