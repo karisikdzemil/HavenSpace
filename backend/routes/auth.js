20e47753c9931b979321c910b404c5e05ccc67ea
dzemil karisik <karisikdzemil@gmail.com>
2025-12-22 21:31:39 +0100

    server side validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,51 @@
-const express = require('express');
-const userRoutes = require('../controllers/user');
+const express = require("express");
+const { body } = require("express-validator");
+const userRoutes = require("../controllers/user");
 
 const router = express.Router();
 
-router.get('/user', );
+router.get("/user", userRoutes.getUser);
 
-router.post('/create-user');
+router.post(
+  "/create-user",
+  [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required!")
+      .isEmail.withMessage("Please enter a valid email!")
+      .normalizeEmail(),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long!")
+      .matches("[0-9]")
+      .withMessage("Password must contain a number!")
+      .matches("[A-Z]")
+      .withMessage("Password must containt an uppercase letter!")
+      .matches("[a-z]")
+      .withMessage("Password must contant a lowercase letter!"),
+  ],
+  userRoutes.createUser
+);
 
-router.delete('/delete-user');
+router.delete("/delete-user", userRoutes.deleteUser);
 
-router.put('/edit-user');
+router.put(
+  "/edit-user",
+  [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required!")
+      .isEmail.withMessage("Please enter a valid email!")
+      .normalizeEmail(),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters long!")
+      .matches("[0-9]")
+      .withMessage("Password must contain a number!")
+      .matches("[A-Z]")
+      .withMessage("Password must containt an uppercase letter!")
+      .matches("[a-z]")
+      .withMessage("Password must contant a lowercase letter!"),
+  ],
+  userRoutes.editUser
+);
