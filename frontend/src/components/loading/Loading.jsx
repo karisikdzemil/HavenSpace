da5903ca0bb97c05a93a87d6925ab8ee18cfc711
dzemil karisik <karisikdzemil@gmail.com>
2025-12-27 19:38:32 +0100

    practive string manipulation

--- frontend/src/components/loading/Loading.jsx
+++ frontend/src/components/loading/Loading.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 
 export default function Loading() {
-
-    const [dots, setDots] = useState('.');
+    // const [dots, setDots] = useState('.');
+    // const dotsLoading = () => {
+    //     if(dots.length === 1){
+    //         setDots(dots => dots += '.' );
+    //     }else if(dots.length === 2){
+    //         setDots(dots => dots += '.' );
+    //     }else if(dots.length === 2){
+    //         setDots(dots => dots = dots.slice(0, -1))
+    //     }else if(dots.length === 3){
+    //         setDots(dots => dots = dots.slice(0, -1));
+    //     }
+    // }
 
   return (
     <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
