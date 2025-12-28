cf5cdd365083c1b6d5989d4a07560ab47679d09e
dzemil karisik <karisikdzemil@gmail.com>
2025-12-28 17:40:53 +0100

    counting dots state

--- frontend/src/components/loading/Loading.jsx
+++ frontend/src/components/loading/Loading.jsx
@@ -1,25 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Loading() {
-    // const [dots, setDots] = useState('.');
-    // const dotsLoading = () => {
-    //     if(dots.length === 1){
-    //         setDots(dots => dots += '.' );
-    //     }else if(dots.length === 2){
-    //         setDots(dots => dots += '.' );
-    //     }else if(dots.length === 2){
-    //         setDots(dots => dots = dots.slice(0, -1))
-    //     }else if(dots.length === 3){
-    //         setDots(dots => dots = dots.slice(0, -1));
-    //     }
-    // }
+  const [dots, setDots] = useState(".");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots(prev =>
+        prev.length === 3 ? "." : prev + "."
+      );
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
       <div className="w-22 h-22 border-4 border-[#1E1E1E] border-t-transparent rounded-full animate-spin" />
-      <p className="text-bold text-2xl text-[#1E1E1E]">
-        Loading elements 
-        <span> {} </span>
+      <p className="font-bold text-2xl text-[#1E1E1E]">
+        Loading elements <span>{dots}</span>
       </p>
     </div>
   );
