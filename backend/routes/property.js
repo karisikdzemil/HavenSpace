9144f54ed95b9cdc7fa1b7d686a16c976ca2fc61
dzemil karisik <karisikdzemil@gmail.com>
2026-01-01 18:22:37 +0100

    added validation logic to edit property

--- backend/routes/property.js
+++ backend/routes/property.js
@@ -93,7 +93,7 @@ router.post(
 
 router.put(
   "/edit-property/:id",
-  [
+   [
     body("title")
       .trim()
       .isLength({ min: 5 })
@@ -103,9 +103,67 @@ router.put(
       .withMessage("The value must be a number!")
       .isInt({ gt: 0 })
       .withMessage("The value must be greater than 0!"),
+    body("location")
+      .exists()
+      .withMessage("Location is required!")
+      .isObject()
+      .withMessage("Location must be an object!"),
+    body("location.city").trim().notEmpty().withMessage("City is required!"),
+    body("location.address")
+      .trim()
+      .notEmpty()
+      .withMessage("Address is required!"),
+    body("location.lat")
+      .notEmpty()
+      .withMessage("Latitude is required!")
+      .isFloat({ min: -90, max: 90 })
+      .withMessage("Latitude must be between -90 and 90"),
+    body("location.lng")
+      .notEmpty()
+      .withMessage("Longitude is required!")
+      .isFloat({ min: -180, max: 180 })
+      .withMessage("Longitude must be between -180 and 180"),
     body("description")
       .isLength({ min: 25 })
       .withMessage("The description must be longer than 25 letters!"),
+    body("type")
+      .notEmpty()
+      .withMessage("Type is required!")
+      .isIn(["rent", "sale"])
+      .withMessage("Type must be either rent or sale"),
+    body("bedNum")
+      .notEmpty()
+      .withMessage("Bedroom number is required!")
+      .isNumeric.withMEssage("Value must be a number!")
+      .isInt({ gt: 0 })
+      .withMessage("The value must be greater than 0!"),
+    body("bathNum")
+      .notEmpty()
+      .withMessage("Bathroom number is required!")
+      .isNumeric.withMEssage("Value must be a number!")
+      .isInt({ gt: 0 })
+      .withMessage("The value must be greater than 0!"),
+    body("area")
+      .notEmpty()
+      .withMessage("Property area is required!")
+      .isNumeric.withMEssage("Value must be a number!")
+      .isInt({ gt: 0 })
+      .withMessage("The value must be greater than 0!"),
+    body("garage").isNumeric().withMessage("Value must be a number!"),
+    body("interiorFeatures")
+      .isArray({ min: 1 })
+      .withMessage("Interior features must an array!"),
+    body("interiorFeatures.*")
+      .trim()
+      .isLength({ min: 3, max: 30 })
+      .withMessage("Each interior feature must be between 2-3 words!"),
+    body("exteriorFeatures")
+      .isArray({ min: 1 })
+      .withMessage("Exterior features must an array!"),
+    body("exteriorFeatures.*")
+      .trim()
+      .isLength({ min: 3, max: 30 })
+      .withMessage("Each exterior feature must be between 2-3 words!"),
   ],
   isAuth,
   propertyController.editProperty
