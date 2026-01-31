1263db4c5474b3df60c62d76b943da9420f1b86f
dzemil karisik <karisikdzemil@gmail.com>
2026-01-31 21:22:36 +0100

    ..

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -8,58 +8,54 @@ export default function Header() {
   const { isAuthenticated, logout } = useAuth();
 
   return (
-      <header className="w-full p-5">
-  <div className="w-full bg-white rounded-[50px] shadow-xl py-[5px] px-[25px] flex items-center justify-between">
-    
-    <h1 className="text-[22px] font-bold text-[#163535]">
-      HavenSpace
-    </h1>
+    <header className="w-full p-5 bg-transparent absolute">
+      <div className="w-full bg-white rounded-[50px] shadow-xl py-2.5  px-[25px] flex items-center justify-between">
+        <h1 className="text-[22px] font-bold text-[#163535]">HavenSpace</h1>
 
-    <nav className="hidden xl:block">
-      <ul className="flex items-center gap-10">
-        <HeaderLinks path="/" text="Home" />
-        <HeaderLinks path="/properties" text="Properties" />
-        <HeaderLinks path="/agents" text="Agents" />
+        <nav className="hidden xl:block">
+          <ul className="flex items-center gap-10">
+            <HeaderLinks path="/" text="Home" />
+            <HeaderLinks path="/properties" text="Properties" />
+            <HeaderLinks path="/agents" text="Agents" />
 
-        {isAuthenticated && (
-          <>
-            <HeaderLinks path="/new-listings" text="New Listings" />
-            <HeaderLinks path="/my-properties" text="My Properties" />
-          </>
-        )}
+            {isAuthenticated && (
+              <>
+                <HeaderLinks path="/new-listings" text="New Listings" />
+                <HeaderLinks path="/my-properties" text="My Properties" />
+              </>
+            )}
+            <HeaderLinks path="/contact" text="Contact" />
+          </ul>
+        </nav>
 
-        <HeaderLinks path="/contact" text="Contact" />
-      </ul>
-    </nav>
-
-    <div className="hidden items-center xl:flex">
-      {isAuthenticated ? (
-        <button
-          onClick={logout}
-          className="text-white bg-[#2c7a7b] text-[14px] py-2 px-5 rounded-[50px] transition-all hover:bg-[#3d9ea0]"
-        >
-          LOGOUT
+        <div className="hidden items-center xl:flex">
+          {isAuthenticated ? (
+            <button
+              onClick={logout}
+              className="text-white bg-[#2c7a7b] text-[14px] py-2 px-5 rounded-[50px] transition-all hover:bg-[#3d9ea0]"
+            >
+              LOGOUT
+            </button>
+          ) : (
+            <NavLink
+              to="/register"
+              className={({ isActive }) =>
+                `text-white text-[14px] py-2 px-5 rounded-[50px] duration-300 transition-all ${
+                  isActive ? "bg-[#3d9ea0]" : "bg-[#2c7a7b] hover:bg-[#3d9ea0]"
+                }`
+              }
+            >
+              Register
+            </NavLink>
+          )}
+        </div>
+        <button className="block xl:hidden cursor-pointer">
+          <FontAwesomeIcon
+            className="text-xl transition-all hover:text-[#3d9ea0]"
+            icon={faBars}
+          />
         </button>
-      ) : (
-        <NavLink
-          to="/register"
-          className={({ isActive }) =>
-            `text-white text-[14px] py-2 px-5 rounded-[50px] duration-300 transition-all ${
-              isActive
-                ? "bg-[#3d9ea0]"
-                : "bg-[#2c7a7b] hover:bg-[#3d9ea0]"
-            }`
-          }
-        >
-          Register
-        </NavLink>
-      )}
-    </div>
-    <button className="block xl:hidden cursor-pointer">
-      <FontAwesomeIcon className="text-xl transition-all hover:text-[#3d9ea0]" icon={faBars} />
-    </button>
-
-  </div>
-</header>
+      </div>
+    </header>
   );
 }
